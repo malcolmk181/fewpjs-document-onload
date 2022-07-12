3879042b4c5d5bc6e1901d87d15cd27e116b733d
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-12 09:22:10 -0400

    commit tree ccd58c50a560a9e21570e6e25b6d13f3e5b15e63

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+    elt = document.getElementById("text");
+    elt.innerHTML = "This is really cool!";
+    // it works in the browser, but not in the test suite
+});
